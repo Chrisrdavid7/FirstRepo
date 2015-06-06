5e8af43858eb7c2b7f3b9c19e44f6a3430b432de
Chris David <chrisdavid@C4Qs-MacBook-Pro.local>
2015-06-06 11:02:07 -0400

    First Commit

---
+++ Twelve Days Of Christmas/Twelve Days Of Christmas/main.m
@@ -0,0 +1,50 @@
+//
+//  main.m
+//  Twelve Days Of Christmas
+//
+//  Created by Chris David on 6/4/15.
+//  Copyright (c) 2015 Chris David. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Twelve Days of Christmas!");
+        
+        NSString *first = @"a Partridge in a Pear Tree";
+        NSString *second = @"Two Turtle Doves";
+        NSString *third = @"Three French Hens";
+        NSString *forth = @"Four Calling Birds";
+        NSString *fifth = @"Five Golden Rings";
+        NSString *sixth = @"Six Grease A-Laying";
+        NSString *seventh= @"Seven Swans of Swimming";
+        NSString *eight= @"Eight Maids A-Milking";
+        NSString *ninth= @"Nine ladies dancing";
+        NSString *tenth= @"Ten Lord-a-Leaping";
+        NSString *eleven= @"Eleven pipers piping";
+        NSString *twelveth= @"Twelve Drummers Drumming";
+        
+        
+        
+        
+        NSLog(@"On the first day of christmas my true love gave to me %@", first);
+        NSLog(@"On the second day of christmas my true love gave to me %@", second);
+        NSLog(@"On the third day of christmas my true love gave to me %@", third);
+        NSLog(@"On the forth day of christmas my true love gave to me %@", forth);
+        NSLog(@"On the fifth day of christmas my true love gave to me %@", fifth);
+        NSLog(@"On the sixth day of christmas my true love gave to me %@", sixth);
+        NSLog(@"On the seventh day of christmas my true love gave to me %@", seventh);
+        NSLog(@"On the eight day of christmas my true love gave to me %@", eight);
+        NSLog(@"On the eight day of christmas my true love gave to me %@", ninth);
+        NSLog(@"On the tenth day of christmas my true love gave to me %@", tenth);
+        NSLog(@"On the eleventh day of christmas my true love gave to me %d", eleven);
+        NSLog(@"On the twelveth day of christmas my true love gave to me %d", twelveth);
+        
+        
+        
+        
+    }
+    return 0;
+}
---
+++ Variables2/Variables2/main.m
@@ -0,0 +1,45 @@
+//
+//  main.m
+//  Variables2
+//
+//  Created by Chris David on 6/4/15.
+//  Copyright (c) 2015 Chris David. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        
+        NSLog(@"*+---------------------------------------------------+");
+        NSLog(@"|%d|                 Accesscode|", a);
+        
+        /*+------------------------------------------------------------+
+        | a |                          Accesscode  |       Mr. Mike  |
+        | b |                         Introduction |      Ms. Aliya  |
+        | c |                         Knicks Rule  |         Carmelo |
+        | d |                          NBA Finials |    Lebron James |
+        | e |                        I Love Music  |     Mr. Spotify |
+        | f |                           I Am A God |     Kanye West  |
+        | g |                       Favorite Movie |     The Notebook|
+        | h | I know this wouldnt work but ill try |            YOLO |
+        +------------------------------------------------------------+");
+       */
+        
+        
+        
+        
+    }
+    return 0;
+}
